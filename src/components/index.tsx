ff3508de0c62d4c56db198604dc511cd92fc3393
kris1027 <krzys89@gmail.com>
2025-08-26 18:20:29 +0200

    refactor: remove old no more used files

--- src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-// Main application components
-export { MainMenu } from './MainMenu';
-export { CVForm } from './CVForm';
-export { CVDisplay } from './CVDisplay';
-
-// Re-export sub-components for convenience
-export * from './ui';
-export * from './layout';
-export * from './cv-sections';
